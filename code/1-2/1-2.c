0983adcc5d22a712d15f8d7e7bdbc2fde15d18e8
yosi475 <yosi475@naver.com>
2026-03-28 11:59:16 +0900

    Update 1-2.c

---
+++ code/1-2/1-2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#include "util.h"
+
+int main() {
+    for(int i = 0; i<100; i++)
+        if(i%2 ==0)
+            logger("even");
+        else
+            logger("odd");
+        
+    return 0;
+}
