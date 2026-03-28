136e672d0bb1a8bcd96714e54fd3dd93f904d094
yosi475 <yosi475@naver.com>
2026-03-28 13:21:03 +0900

    Update 3-2.c

--- code/3-2/3-2.c
+++ code/3-2/3-2.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 
 void swap_endian(int *x) {
-    
+    unsigned char *ptr = (unsigned char *)x;
+    unsigned char temp;
+    temp = ptr[0];
+    ptr[0] = ptr[3];
+    ptr[3] = temp;
+    temp = ptr[1];
+    ptr[1] = ptr[2];
+    ptr[2] = temp;
 }
 
 int main() {
     int x = 0x12345678;
-
+    printf("%x\n", x); 
+    swap_endian(&x); 
     printf("%x\n", x);
-    swap_endain(&x);
-    printf("%x\n", x);
-
     return 0;
 }
