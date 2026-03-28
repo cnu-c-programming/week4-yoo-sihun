43b8a403a877032401a6c8a26969822b0f1e7963
yosi475 <yosi475@naver.com>
2026-03-28 12:18:39 +0900

    Update 3-3.c

---
+++ code/3-3/3-3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("sizeof(int*) = %zu\n", sizeof(int*));
+    printf("sizeof(double*) = %zu\n", sizeof(double*));
+    printf("sizeof(char*) = %zu\n", sizeof(char*));
+    printf("sizeof(void*) = %zu\n", sizeof(void*));
+
+    return 0;
+}
